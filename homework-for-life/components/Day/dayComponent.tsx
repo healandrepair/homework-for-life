cc4feb4ad5203bf02c2ab92f0009dc72bd9ee7de
healandrepair <healandrepair@outlook.com>
2024-08-10 16:26:06 +1200

    push

---
+++ homework-for-life/components/Day/dayComponent.tsx
@@ -0,0 +1,9 @@
+function dayComponent() {
+    return (
+        <div>
+            <h1>Day Component</h1>
+        </div>
+    );
+}
+
+export default dayComponent;
---
+++ homework-for-life/src/pages/calendar.tsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/calendar.module.css';
+
+function calendar() {
+    return (
+        <div className={styles.container}>
+            <div>
+                Hi
+            </div>
+            <div>
+                World
+            </div>
+        </div>
+    );
+}
+
+export default calendar;
